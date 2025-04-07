7807b4a1a042996f2bc800c3bf37114ff7e26701
Ardadagdelenn <ad24544@essex.ac.uk>
2025-04-07 04:10:07 +0100

    Added a Coordinates class

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,22 @@
-public class Shape {
+public abstract class Shape {
+    protected Coordinates position;
+    protected int sides;
+
+    public Shape(Coordinates position, int sides) {
+        this.position = position;
+        this.sides = sides;
+    }
+
+    public Coordinates getCoordinates() { return position; }
+    public int getSides() { return sides; }
+    public void setCoordinates(Coordinates position) { this.position = position; }
+
+    public void translate(int dx, int dy) {
+        position.translate(dx, dy);
+    }
+
+    public abstract void scale(int factor, boolean sign);
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    public abstract void display();
 }
