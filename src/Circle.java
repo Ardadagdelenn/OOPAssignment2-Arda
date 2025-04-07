59ec86ac196bc86efe8f7b16c18e94dfda7d8341
Ardadagdelenn <ad24544@essex.ac.uk>
2025-04-07 04:14:09 +0100

    Circle class added.

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,30 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(Coordinates position, double radius) {
+        super(position, 0);
+        if (radius <= 0) throw new IllegalArgumentException("Radius must be positive.");
+        this.radius = radius;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        if (factor <= 0) throw new IllegalArgumentException("Scale factor must be positive.");
+        radius = sign ? radius * factor : radius / factor;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Circle: Center (" + position.getX() + ", " + position.getY() + "), Radius: " + radius);
+    }
 }
